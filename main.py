3aefd7cabb36428fdd759abd5c97177157736ac9
DillonNMorse <DillonNMorse@gmail.com>
2020-10-05 16:34:17 -0600

    Update tp include second processing pass

--- main.py
+++ main.py
@@ -133,9 +133,9 @@ def main(img_filepath, save_filepath):
     lin_segments =  f.keep_linear_only(edge_coords,
                                        distances,
                                        num_neighbors = 21,
-                                       error_thresh = 0.8,
+                                       error_thresh = 1.5,
                                        slope_thresh = 6,
-                                       cut_y_val = 110,
+                                       cut_y_val = 125,
                                       )
     
     fig, ax = plt.subplots(1,1)
@@ -180,7 +180,7 @@ def main(img_filepath, save_filepath):
     
     ax.set_title('Floor-Wall Boundary\n(depth measured in feet)')
     
-    plt.savefig(save_filepath + '_depth.jpg')
+    plt.savefig(save_filepath + '_depthimage.jpg')
     
     
     
@@ -240,6 +240,159 @@ def main(img_filepath, save_filepath):
     #plt.axis('off')
     plt.savefig(save_filepath + '_map.jpg')
 
+    
+    
+    
+    # =========================================================================
+    # Use above info to identify floor clearly from semantics, repeat.
+    # =========================================================================
+    
+
+    
+    bbox = f.bounding_box(lin_segments)
+    floor_class = f.dominant_class(bbox, semantics )
+    masked_semantics = f.mask_semantics(semantics, floor_class)
+    
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(masked_semantics, cmap = 'gray_r')
+    #plt.axis('off')
+    plt.savefig(save_filepath + '_masked_semantics.jpg')    
+    
+    
+    edges2 = f.edges_from_semantics(masked_semantics)
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(edges2)
+    plt.savefig(save_filepath + '_Edges2.jpg')
+    
+    # =============================================================================
+    # Extract coordinates along all edges of the different segmentation classes. 
+    #   Keep only those points that which are (i) in an approximately-linear
+    #   boundary segment, (ii) have a slope less than some threshold value (want 
+    #   to drop any vertical lines), and (iii) are below a critical y-value 
+    #   (don't  want to capture the ceiling-wall interface).
+    # =============================================================================
+    edge_coords2 = f.extract_coords(edges2, strip_size = 10)
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter( edge_coords2[:,0], edge_coords2[:,1] )
+    plt.savefig(save_filepath + '_Edge2_Coords_Original.jpg')
+    
+    distances2 = f.coord_distances(edge_coords2)
+    
+    lin_segments2 =  f.keep_linear_only(edge_coords2,
+                                       distances2,
+                                       num_neighbors = 22,
+                                       error_thresh = 1.5,
+                                       slope_thresh = 1000,
+                                       cut_y_val = 90,
+                                      )
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter( lin_segments2[:,0], lin_segments2[:,1] )
+    ax.set_xlim([0,256])
+    ax.set_ylim([0,192])
+    ax.invert_yaxis()
+    plt.savefig(save_filepath + '_Linear_Segments2.jpg')
+    
+    # =============================================================================
+    # Cluster remaining linear segments by relative proximity, fit each cluster
+    #   to a line, and extrapolate those lines until they intersect. Finallly,
+    #   map the depth data on to the newly-created boundary lines.
+    # =============================================================================
+    clusters2 = f.cluster_lin_segments(lin_segments2)
+    boundary2 = f.interpolate_clusters(lin_segments2, clusters2)
+    depth_boundary2 = f.map_depth_to_boundary(boundary2, depth)
+    
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(boundary2[:,0], boundary2[:,1] )
+    ax.set_xlim([0,256])
+    ax.set_ylim([0,192])
+    ax.invert_yaxis()
+    plt.savefig(save_filepath + '_Boundary2.jpg')
+    
+    # =============================================================================
+    # Make plot
+    # =============================================================================
+    m_to_ft = 3.28084
+    fig, ax = plt.subplots(1,1)
+    depth_plot = ax.scatter( boundary2[:,0]*(w_scale),
+                             boundary2[:,1]*(h_scale),
+                             c = depth_boundary2*m_to_ft,
+                             cmap = 'YlGnBu_r',
+                             alpha = 0.5
+                           )
+    ax.imshow(img)
+    
+    plt.colorbar(depth_plot)
+    ax.axis('tight')
+    ax.axis('off')
+    
+    ax.set_title('Floor-Wall Boundary\n(depth measured in feet)')
+    
+    plt.savefig(save_filepath + '_depthimage2.jpg')
+    
+
+
+    # =============================================================================
+    # Find perspective transformation for approx-rectangular rooms.
+    # =============================================================================
+    
+    
+    
+    corners = f.get_persp_corners(boundary2, im_w, im_h)
+    
+    tl = corners[0,:]
+    tr = corners[1,:]
+    br = corners[2,:]
+    bl = corners[3,:]
+    
+    
+    top_x = [tl[0], tr[0]]
+    left_x = [bl[0], tl[0]]
+    right_x = [br[0], tr[0]]
+    
+    top_y = [tl[1], tr[1]]
+    left_y = [bl[1], tl[1]]
+    right_y = [br[1], tr[1]]
+    
+    
+    fig3, ax3 = plt.subplots(1,1)
+    
+    ax3.plot(top_x, top_y )
+    ax3.plot(left_x, left_y )
+    ax3.plot(right_x, right_y )
+    
+    plt.imshow(img)
+    plt.savefig(save_filepath + '_persp_outline2.jpg')
+
+    
+    dst_pts = f.target_corners(h = 19*50, w = 12*50)
+    
+    
+    result1 = f.perspective_shift(img, corners, dst_pts) 
+    fig4, (ax4) = plt.subplots(1,1)
+    fig.tight_layout()
+    ax4.imshow(result1)
+    
+    
+    
+    boundary_scaled2 = np.array([(boundary2[k,0]*w_scale, boundary2[k,1]*h_scale) for k in range(  boundary2.shape[0] )] )
+    
+    boundary_img2 = f.boundary_to_img(boundary_scaled2, h = 192*h_scale, w = 256*w_scale)
+    
+    result22 = f.perspective_shift(boundary_img2, corners, dst_pts) 
+    
+    
+    fig, ax = plt.subplots(1,1)
+    ax.imshow(result22, cmap = 'gray_r')
+    #plt.axis('off')
+    plt.savefig(save_filepath + '_map2.jpg')    
+    
+    
+    
+    
+    
+
 
 
     return (depth_boundary.max() - 0*depth_boundary.min())*m_to_ft
@@ -259,7 +412,7 @@ if __name__ == '__main__':
     # =============================================================================
     def create_filepath(im_name, append = '', type = 'folder' ):
         
-        save_path = 'F:/RoomRotate/Processed/' 
+        save_path = 'F:/Insight/RoomPerspective/Processed/' 
         save_filepath = save_path + im_name + '/'
         
         if type == 'file':
